0a06b45789fca9e05a7432ed560213369c3bff48
Adrian Niculescu <adriann@intern.pch>
2021-09-26 23:40:14 +0300

    Finished L3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
+  <div class="events">
+    <h1>Events for Good</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+      {
+        id: 5928101,
+        category: 'aimal welfare',
+        title: 'Cat Adoption Day',
+        description: 'Find your new feline friend',
+        location: 'Meow Town',
+        date: 'Jan 28, 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organizer: 'Kat Laydee'
+      },
+      {
+        id: 5928102,
+        category: 'food',
+        title: 'Community Gardening',
+        description: 'Find your new feline friend',
+        location: 'Meow Town',
+        date: 'Jan 28, 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organizer: 'Kat Laydee'
+      },
+            {
+        id: 4828102,
+        category: 'another',
+        title: 'Community Gardening',
+        description: 'Find your new feline friend',
+        location: 'Meow Town',
+        date: 'Jan 21, 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organizer: 'Kat Laydee'
+      }
+      ]
+    }
   }
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
